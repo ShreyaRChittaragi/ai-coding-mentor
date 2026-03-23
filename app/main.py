478cd861a30e14292523ea5d10945e720a125e8d
Shreya R Chittaragi <shreyarchittaragi24@gmail.com>
2026-03-23 14:31:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
-from app.api.routes import visualizations  
 from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv()
-
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.api.routes import memory
 from app.api.routes import submit_code, get_problem, get_feedback, user_profile
 from app.api.routes import next_problem
+from app.api.routes import visualizations
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -21,7 +20,10 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
+        "https://ai-coding-mentor-dd7ndbh9x-shreyarchittaragis-projects.vercel.app",
+        "https://*.vercel.app",
     ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -32,6 +34,7 @@ app.include_router(get_problem.router)
 app.include_router(get_feedback.router)
 app.include_router(user_profile.router)
 app.include_router(next_problem.router)
+app.include_router(visualizations.router)
 
 @app.get("/")
 def root():
@@ -40,5 +43,3 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "healthy"}
-       
-app.include_router(visualizations.router)      
